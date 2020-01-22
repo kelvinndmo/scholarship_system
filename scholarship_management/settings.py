45e09967d62879ec7ccc7175dfbcf17c1d6a0aa0
novak <ndemo.kelvin@students.jkuat.ac.ke>
2020-01-22 09:26:23 +0300

    project files

--- scholarship_management/settings.py
+++ scholarship_management/settings.py
@@ -149,10 +149,19 @@ EMAIL_USE_TLS = True
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = EMAIL_HOST_USER
 '''END EMAIL CONFIGURATION'''
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_landville'
+        }
+    },
 }
 
 
